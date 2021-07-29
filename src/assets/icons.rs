e75312445ec94e129739fb9920d6e8281daf78a7
MiniaczQ <jakub.motyka.2000@gmail.com>
2021-07-29 18:08:55 +0200

    fixed docs

--- src/assets/icons.rs
+++ src/assets/icons.rs
@@ -1,13 +1,10 @@
-/*
-For loading and validating images and turning them into base64 server icons.
-*/
-
 use std::io::Write;
 
 use image::{DynamicImage, GenericImageView};
 
 type OptIcon = Option<String>;
 
+/// Server icon assets.
 #[derive(Clone)]
 pub struct ServerIcons {
     pub no_hosts: OptIcon,
@@ -18,10 +15,8 @@ impl ServerIcons {
     const NO_HOSTS: &'static str = "assets/icons/NoHosts.png";
     const MANY_HOSTS: &'static str = "assets/icons/ManyHosts.png";
 
-    /*
-    Encodes the image in b64.
-    */
-    fn encode(image: DynamicImage) -> Option<String> {
+    /// Encode image in base 64.
+    fn encode(image: DynamicImage) -> OptIcon {
         let mut png: Vec<u8> = Vec::new();
         image.write_to(&mut png, image::ImageOutputFormat::Png).expect("Failed to encode image as PNG.");
         let mut encoder = base64::write::EncoderStringWriter::new(base64::STANDARD_NO_PAD);
@@ -29,11 +24,9 @@ impl ServerIcons {
         return Some(encoder.into_inner())
     }
 
-    /*
-    Validates image with server icon requirements. (64 x 64)
-    Returns the encoded image.
-    */
-    fn encode_valid(name: String, image: DynamicImage) -> Option<String> {
+    /// Validate image with server icon requirements. (64 x 64)
+    /// Return the image encoded.
+    fn encode_valid(name: String, image: DynamicImage) -> OptIcon {
         if image.dimensions() == (64, 64) {
             return Self::encode(image)
         }
@@ -41,10 +34,8 @@ impl ServerIcons {
         None
     }
 
-    /*
-    Loads, validates and encodes one image as icon.
-    */
-    fn get_icon(name: String, path: &String) -> Option<String> {
+    /// Load, validate and encode image.
+    fn get_icon(name: String, path: &String) -> OptIcon {
         let result = image::open(path);
         if let Ok(image) = result {
             return Self::encode_valid(name, image)
@@ -53,9 +44,7 @@ impl ServerIcons {
         None
     }
 
-    /*
-    Loads all images into an icon list.
-    */
+    /// Load all icons.
     pub fn get_icons() -> Self {
         Self {
             no_hosts: Self::get_icon("'No hosts' image".to_string(),  &ServerIcons::NO_HOSTS.to_string()),
